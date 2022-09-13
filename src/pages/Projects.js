8358134009241a8d91dff2ea00966d3e7a736646
bbakercello <bbakercello@gmail.com>
2022-09-13 14:05:28 -0400

    removed some errors from the notion

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,36 +5,38 @@ function Projects(props) {
   // create state to hold projects
   const [projects, setProjects] = useState(null);
 
-  //create function to make api call
-  const getProjectsData = async () => {
-    
-		//make api call and get response
-    const response = await fetch(props.URL + "projects");
-    
-		// turn response into javascript object
-    const data = await response.json();
-    
-		// set the projects state to the data
-    setProjects(data);
 
-  };
 
-  // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getProjectsData(), []);
+ // create function to make api call
+ const getProjectsData = async () => {
+    
+  // make api call and get response
+  const response = await fetch(props.URL + "Projects");
+  
+  // turn response into javascript object
+  const data = await response.json();
+  
+  // set the about state to the data
+  setProjects(data);
+};
+
+// make an initial call for the data inside a useEffect, so it only happens once on component load
+useEffect(() => {getProjectsData()}, []);
+
 
   // define a function that will return the JSX needed once we get the data
-  const loaded = () => {
-    return projects.map((project) => (
-      <div>
-        <h1>{project.name}</h1>
-        <img src={project.image} />
-        <a href={project.git}>
-          <button>Github</button>
-        </a>
-        <a href={project.live}>
-          <button>live site</button>
-        </a>
-      </div>
+const loaded = () => {
+  return projects.map((project) => (
+    <div>
+      <h1>{projects.name}</h1>
+      <img src={projects.image} alt="project"/>
+      <a href={projects.git}>
+      <button>Github</button>
+      </a>
+      <a href={projects.live}>
+                <button>live site</button> 
+      </a>
+    </div>
     ));
   };
 
