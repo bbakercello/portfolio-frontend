89dfd6114ee0ec464b5829946fb2603dc1aee336
bbakercello <bbakercello@gmail.com>
2022-09-13 12:17:45 -0400

    components folder created

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <H1>Footer</H1>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <H1>Header</H1>
+  )
+}
+
+export default Header
